d5692fd8eb08a4f4292c7d302daa8009885158b5
Nestor Alvarez <nesty07largmail.com>
2022-11-12 15:02:10 -0400

    Step Two software HTML and JS

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,15 @@
+const divWelcomeContainer = document.querySelector('.formulas-welcome-container');
+const mainTittle = document.querySelector('h1');
+const mainSubTittleOne = document.querySelector('#main-subtittle-one');
+const startButton = document.querySelector('#start-button');
+const divSelectionContainer = document.querySelector('.formulas-selection-container');
+
+startButton.addEventListener('click', () => {
+    divWelcomeContainer.classList.replace('formulas-welcome-container','inactive');
+    divSelectionContainer.classList.remove('inactive');
+    mainTittle.classList.add('inactive');
+    mainSubTittleOne.classList.remove('inactive');
+});
 buttonResult.addEventListener('click', balanceMoneyFormula)
 buttonResult.addEventListener('click', harvEkerFormula)
 buttonResult.addEventListener('click', daveRamseyFormula)
