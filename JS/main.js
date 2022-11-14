0da8af1f0f88cbfc5cdf0af6c18aa9dd93d8dd90
Nestor Alvarez <nesty07largmail.com>
2022-11-14 15:15:23 -0400

    Step two software styles

--- JS/main.js
+++ JS/main.js
@@ -2,11 +2,11 @@ const divWelcomeContainer = document.querySelector('.formulas-welcome-container'
 const mainTittle = document.querySelector('h1');
 const mainSubTittleOne = document.querySelector('#main-subtittle-one');
 const startButton = document.querySelector('#start-button');
-const divSelectionContainer = document.querySelector('.formulas-selection-container');
+const divSelectionContainer = document.querySelector('#software-02');
 
 startButton.addEventListener('click', () => {
     divWelcomeContainer.classList.replace('formulas-welcome-container','inactive');
-    divSelectionContainer.classList.remove('inactive');
+    divSelectionContainer.classList.replace('inactive', 'formulas-selection-container');
     mainTittle.classList.add('inactive');
     mainSubTittleOne.classList.remove('inactive');
 });
