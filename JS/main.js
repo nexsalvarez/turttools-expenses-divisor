a842a8be23143c6722e1ef97be87b2893395979a
Nestor Alvarez <nesty07largmail.com>
2022-11-17 16:07:16 -0400

    Step three software HMTL and JS

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,13 @@
 const divWelcomeContainer = document.querySelector('.formulas-welcome-container');
 const mainTittle = document.querySelector('h1');
 const mainSubTittleOne = document.querySelector('#main-subtittle-one');
+const mainSubTittleTwo = document.querySelector('#main-subtittle-two');
 const startButton = document.querySelector('#start-button');
 const divSelectionContainer = document.querySelector('#software-02');
+const typeBestMoney = document.querySelector('#type-one');
+const typeHarvEker = document.querySelector('#type-two');
+const typeDaveRamsey = document.querySelector('#type-three');
+const divQuestionContainer = document.querySelector('#software-03');
 
 startButton.addEventListener('click', () => {
     divWelcomeContainer.classList.replace('formulas-welcome-container','inactive');
@@ -10,6 +15,35 @@ startButton.addEventListener('click', () => {
     mainTittle.classList.add('inactive');
     mainSubTittleOne.classList.remove('inactive');
 });
-buttonResult.addEventListener('click', balanceMoneyFormula)
-buttonResult.addEventListener('click', harvEkerFormula)
-buttonResult.addEventListener('click', daveRamseyFormula)
+typeBestMoney.addEventListener('click', () => {
+    divSelectionContainer.classList.replace('formulas-selection-container','inactive');
+    divQuestionContainer.classList.replace('inactive', 'formulas-question-container');
+    mainSubTittleOne.classList.add('inactive');
+    mainSubTittleTwo.classList.remove('inactive');
+    formulaSelected('BalanceMoney');
+});
+typeHarvEker.addEventListener('click', () => {
+    divSelectionContainer.classList.replace('formulas-selection-container','inactive');
+    divQuestionContainer.classList.replace('inactive', 'formulas-question-container');
+    mainSubTittleOne.classList.add('inactive');
+    mainSubTittleTwo.classList.remove('inactive');
+    formulaSelected('HarvEker');
+});
+typeDaveRamsey.addEventListener('click', () => {
+    divSelectionContainer.classList.replace('formulas-selection-container','inactive');
+    divQuestionContainer.classList.replace('inactive', 'formulas-question-container');
+    mainSubTittleOne.classList.add('inactive');
+    mainSubTittleTwo.classList.remove('inactive');
+});
+
+function formulaSelected (formula) {
+    if (formula == 'BalanceMoney') {
+        return balanceMoneyFormula();
+    } else if (formula == 'HarvEker') {
+        return harvEkerFormula();
+    } else {
+        return daveRamseyFormula();
+    }
+}
+
+// buttonResult.addEventListener('click', balanceMoneyFormula)
